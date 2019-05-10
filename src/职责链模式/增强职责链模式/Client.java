be57fe7bbb36bc7f1afc341627732d7774ac238f
wangxizzz <979675820@qq.com>
2019-05-10 22:26:39 +0800

    加深职责模式

---
+++ src/职责链模式/增强职责链模式/Client.java
@@ -0,0 +1,14 @@
+package 职责链模式.增强职责链模式;
+
+/**
+ * <Description>
+ *
+ * @author wangxi
+ */
+public class Client {
+    public static void main(String[] args) {
+        //DepManager的实现与ProjectManager类似
+
+    }
+}
+
