d9d7b581b33507fcecda4655336aca3d79d66d84
wangxizzz <979675820@qq.com>
2019-08-15 23:36:32 +0800

    同步获取数据

--- refactoring/src/main/java/com/code/refactoring/zookeeper/book/chapter05/java客户端操作/$5_3_4/GetData_API_Sync_Usage.java
+++ refactoring/src/main/java/com/code/refactoring/zookeeper/book/chapter05/java客户端操作/$5_3_4/GetData_API_Sync_Usage.java
@@ -1,9 +1,8 @@
 package com.code.refactoring.zookeeper.book.chapter05.java客户端操作.$5_3_4;
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
+
+import org.apache.zookeeper.*;
 import org.apache.zookeeper.Watcher.Event.EventType;
 import org.apache.zookeeper.Watcher.Event.KeeperState;
-import org.apache.zookeeper.ZooKeeper;
 import org.apache.zookeeper.data.Stat;
 
 import java.util.concurrent.CountDownLatch;
@@ -17,32 +16,34 @@ public class GetData_API_Sync_Usage implements Watcher {
 
     public static void main(String[] args) throws Exception {
 
-    	String path = "/zk-book";
-    	zk = new ZooKeeper("localhost:2181",
-				5000, //
-				new GetData_API_Sync_Usage());
+        String path = "/zk-book";
+        zk = new ZooKeeper("localhost:2181",
+                5000, //
+                new GetData_API_Sync_Usage());
         connectedSemaphore.await();
-        //zk.create( path, "123".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL );
-        
-        System.out.println(new String(zk.getData( path, true, stat )));
-        System.out.println(stat.getCzxid()+","+stat.getMzxid()+","+stat.getVersion());
-        
-        zk.setData( path, "123".getBytes(), -1 );
-        
-        Thread.sleep( Integer.MAX_VALUE );
+        zk.create(path, "452353".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+        // 获取节点数据并且注册一个默认的监听(由类实现的监听process)
+        System.out.println(new String(zk.getData(path, true, stat)));
+        System.out.println(stat.getCzxid() + "," + stat.getMzxid() + "," + stat.getVersion());
+
+        zk.setData(path, "123".getBytes(), stat.getVersion());
+
+        Thread.sleep(Integer.MAX_VALUE);
     }
+
     public void process(WatchedEvent event) {
         if (KeeperState.SyncConnected == event.getState()) {
-  	      if (EventType.None == event.getType() && null == event.getPath()) {
-  	          connectedSemaphore.countDown();
-  	      } else if (event.getType() == EventType.NodeDataChanged) {
-  	          try {
-  	              System.out.println(new String(zk.getData( event.getPath(), true, stat )));
-  	              System.out.println(stat.getCzxid()+","+
-  	                                 stat.getMzxid()+","+
-  	            		             stat.getVersion());
-  	          } catch (Exception e) {}
-  	      }
-  	    }
-       }
+            if (EventType.None == event.getType() && null == event.getPath()) {
+                connectedSemaphore.countDown();
+            } else if (event.getType() == EventType.NodeDataChanged) {
+                try {
+                    System.out.println(new String(zk.getData(event.getPath(), true, stat)));
+                    System.out.println(stat.getCzxid() + "," +
+                            stat.getMzxid() + "," +
+                            stat.getVersion());
+                } catch (Exception e) {
+                }
+            }
+        }
+    }
 }
