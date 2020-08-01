415c198dfbbe7775d7702dd98ac79e1e4b0aeebb
wangxi <wangxi03@koolearn-inc.com>
2020-08-01 22:16:25 +0800

    fix

--- refactoring/src/main/java/com/code/refactoring/rocketmq/demo/StringConsumerNewNS.java
+++ refactoring/src/main/java/com/code/refactoring/rocketmq/demo/StringConsumerNewNS.java
@@ -1,18 +1,18 @@
-package com.code.refactoring.rocketmq.demo;
-
-import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
-import org.apache.rocketmq.spring.core.RocketMQListener;
-import org.springframework.stereotype.Service;
-
-/**
- * @author wangxi created on 2020/6/26 18:16
- * @version v1.0
- */
-@Service
-@RocketMQMessageListener(nameServer = "${rocketmq.nameserver}", topic = "topicTest", consumerGroup = "string_consumer")
-public class StringConsumerNewNS implements RocketMQListener<String> {
-    @Override
-    public void onMessage(String message) {
-        System.out.printf("------- StringConsumerNewNS received: %s \n", message);
-    }
-}
+//package com.code.refactoring.rocketmq.demo;
+//
+//import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+//import org.apache.rocketmq.spring.core.RocketMQListener;
+//import org.springframework.stereotype.Service;
+//
+///**
+// * @author wangxi created on 2020/6/26 18:16
+// * @version v1.0
+// */
+//@Service
+//@RocketMQMessageListener(nameServer = "${rocketmq.nameserver}", topic = "topicTest", consumerGroup = "string_consumer")
+//public class StringConsumerNewNS implements RocketMQListener<String> {
+//    @Override
+//    public void onMessage(String message) {
+//        System.out.printf("------- StringConsumerNewNS received: %s \n", message);
+//    }
+//}
