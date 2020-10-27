5117acd0c7ab0179bbb771a7aa35fd6828c4b34e
wangxi <wangxi03@koolearn-inc.com>
2020-10-27 15:19:30 +0800

    condition使用

--- refactoring/src/main/java/com/code/refactoring/conditional相关/Conditional注解/BeanConfig.java
+++ refactoring/src/main/java/com/code/refactoring/conditional相关/Conditional注解/BeanConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
  * @author wangxi03 created on 2020/10/27 2:40 PM
  * @version v1.0
  */
+// Conditional 可以设置在类上
 @Configuration
 public class BeanConfig {
 //    @Bean(name = "bill")
